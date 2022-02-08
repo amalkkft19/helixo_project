a13c5440291cd8654b5fd4caec51db402f1dd2f2
Amal K <amalkkft19@gmail.com>
2022-02-08 17:50:36 +0530

    fixed css issues

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
+import { makeStyles } from "@mui/styles";
 
-const NavBar = () => {
+const useStyles = makeStyles({
+  navbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#fff",
+    height: "50px",
+    /* position: fixed; */
+    width: "100%",
+    zIndex: " 10",
+
+    boxShadow: " 0 2px 4px 0 rgba(0, 0, 0, 0.2)",
+  },
+});
+
+const NavBar = (props) => {
+  const classes = useStyles();
   return (
-    <div id="navbar_custom">
+    <div className={classes.navbar}>
       <Link className="nav_id" to="/">
         Home
       </Link>
